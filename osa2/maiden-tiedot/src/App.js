4ad0ec965c41f6b3b31b56e2634375cff0a067a0
Eetu Rantala <rantala.eetu@gmail.com>
2019-07-03 16:35:15 +0300

    removed empty space

--- osa2/maiden-tiedot/src/App.js
+++ osa2/maiden-tiedot/src/App.js
@@ -2,11 +2,6 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Countries from './components/Countries'
 
-
-
-
-
-
 const App = () => {
   const [countries, setCountries] = useState([])
   const [filter, setFilter] = useState("")
