583be6101023aee5be587610eddaf594fc2c04d7
Eetu Rantala <rantala.eetu@gmail.com>
2019-07-09 20:05:01 +0300

    tehty puhelinluettelon backend step8

--- osa3/puhelinluettelo-backend/index.js
+++ osa3/puhelinluettelo-backend/index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 app.use(bodyParser.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+morgan.token('body', (req) => {
+  if(req.method === 'POST') {
+    return JSON.stringify(req.body)
+  }
+})
 
 let persons = [
     {
